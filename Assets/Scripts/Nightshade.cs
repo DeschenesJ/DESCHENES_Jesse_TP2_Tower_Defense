0c3b68eef2f002caf7caa086e4b50e8b7bd79b37
DeschenesJ <101338648+DeschenesJ@users.noreply.github.com>
2022-04-05 14:19:39 -0400

    Update Nightshade.cs

--- Assets/Scripts/Nightshade.cs
+++ Assets/Scripts/Nightshade.cs
@@ -7,17 +7,6 @@ public class Nightshade : Ennemies
 {
     int pvEnnemi = 2;
 
-    void Update()
-    {
-
-        //Set la destination de la target
-        agent.SetDestination(vecDestination);
-        
-
-
-
-
-    }
 
     // Lorsque la nightshade entre dans un box collider des shortcut
     private void OnTriggerEnter(Collider other)
