162241c812fb7663e5444f42d79ab642650e41ae
DeschenesJ <101338648+DeschenesJ@users.noreply.github.com>
2022-04-05 14:45:07 -0400

    Game Manager
    Commit pour le game manager partie 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject spawnpoint;
+    public Transform spawnpoint;
     public GameObject ennemiS;
     public GameObject ennemiN;
     public GameObject ennemiW;
